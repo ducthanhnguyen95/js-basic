dfedf42a362e6e1ee0fe26ff43c8bb825af4ca12
thanhnd <ducthanhnguyen95@gmail.com>
2025-03-02 13:04:09 +0700

    Objects

---
+++ objects.js
@@ -0,0 +1,22 @@
+let name = 'Thanh';
+let age = 30;
+
+let person = {
+  name: 'Thanh',
+  age: 30,
+};
+
+console.log(person);
+console.log(typeof person);
+
+// Dot Notation
+person.name = 'Mosh';
+console.log(person.name);
+
+// Bracket Notation
+person['name'] = 'Mary';
+console.log(person.name);
+
+let selection = 'name';
+person[selection] = 'John';
+console.log(person.name);
