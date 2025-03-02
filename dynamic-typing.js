7e51941cace554641a34992df8ee7e50feca707d
thanhnd <ducthanhnguyen95@gmail.com>
2025-03-02 12:43:25 +0700

    Dynamic typing

---
+++ dynamic-typing.js
@@ -0,0 +1,17 @@
+let name = 'Thanh'; // String Literal
+let age = 30; // Number Literal
+let isApprove = false; // Boolean Literal
+let firstName = undefined;
+let selectedColor = null;
+
+console.log(typeof name);
+name = 1;
+console.log(typeof name);
+
+console.log(typeof age);
+age = 30.5;
+console.log(typeof age);
+
+console.log(typeof isApprove);
+console.log(typeof firstName);
+console.log(typeof selectedColor);
