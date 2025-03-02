6181ad060cdaee1d6839375d815e1602d0cafb80
thanhnd <ducthanhnguyen95@gmail.com>
2025-03-02 11:59:16 +0700

    Variables

---
+++ variables.js
@@ -0,0 +1,15 @@
+// let name;
+// console.log(name);
+let variables = 'Thanh';
+console.log(variables);
+
+// Cannot be a reserved keyword
+// Should be meaningfull
+// Cannot start with a number
+// Cannot contain a space or hyphen (-)
+// Are case-sensitive
+
+let firstName = 'Nguyen Duc';
+let lastName = 'Thanh';
+console.log(firstName);
+console.log(lastName);
