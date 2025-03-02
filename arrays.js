aaf559eb9decf9b689135d6d75f930aa2bf4e96c
thanhnd <ducthanhnguyen95@gmail.com>
2025-03-02 13:22:55 +0700

    Arrays

---
+++ arrays.js
@@ -0,0 +1,9 @@
+let selectedColor = ['red', 'blue'];
+console.log(selectedColor);
+console.log(selectedColor[0]);
+selectedColor[2] = 'green';
+console.log(selectedColor);
+selectedColor[3] = 1;
+console.log(selectedColor);
+console.log(typeof selectedColor);
+console.log(selectedColor.length);
