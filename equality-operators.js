c0c9fa499159ed7490098e36a18e59e6ce4d6716
thanhnd <ducthanhnguyen95@gmail.com>
2025-03-04 22:18:35 +0700

    Equality operators

---
+++ equality-operators.js
@@ -0,0 +1,10 @@
+// Strict Equality (Type + Value)
+console.log(1 === 1);
+console.log('1' === 1);
+
+// Lose Equality (Value)
+console.log(1 == 1);
+console.log('1' == 1);
+// console.log('1' == '1');
+console.log(true == 1);
+// console.log(true == true);
