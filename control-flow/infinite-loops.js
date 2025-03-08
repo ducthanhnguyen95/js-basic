a1bd61172badde9c9ebce656481f128f78c94dc2
thanhnd <ducthanhnguyen95@gmail.com>
2025-03-08 18:22:01 +0700

    Infinite loops

---
+++ control-flow/infinite-loops.js
@@ -0,0 +1,16 @@
+let i = 0;
+while (i < 5) {
+  console.log(i);
+  //   i++;
+}
+
+while (true) {}
+
+do {} while (true);
+
+let x = 0;
+do {} while (x < 5);
+
+for (let i = 0; i >= 0; i++) {}
+for (let i = 0; i < 10; ) {}
+for (let i = 0; true; ) {}
