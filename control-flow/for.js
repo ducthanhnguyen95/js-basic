de74f1b0e5a0edd02131d8da6bb59071a780ab94
thanhnd <ducthanhnguyen95@gmail.com>
2025-03-08 17:07:47 +0700

    For

---
+++ control-flow/for.js
@@ -0,0 +1,15 @@
+// for (initialExpression; condition; incrementExpression) {
+//   statement;
+// }
+
+// for (let i = 0; i < 5; i++) {
+//   console.log('Hello World', i);
+// }
+
+// for (let i = 1; i <= 5; i++) {
+//   if (i % 2 !== 0) console.log(i);
+// }
+
+for (let i = 5; i >= 0; i--) {
+  if (i % 2 !== 0) console.log(i);
+}
