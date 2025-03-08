71662bf94bd98b0ed7808508e04d9c81faf164fd
thanhnd <ducthanhnguyen95@gmail.com>
2025-03-08 18:33:35 +0700

    For in

---
+++ control-flow/for-in.js
@@ -0,0 +1,8 @@
+const person = {
+  name: 'Thanh',
+  age: 30,
+};
+for (let key in person) console.log(key, person[key]);
+
+const colors = ['red', 'green', 'blue'];
+for (let index in colors) console.log(index, colors[index]);
