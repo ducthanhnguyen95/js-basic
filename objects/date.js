161f46ab7746c822f7309a264f3d0c1e3a0566ef
thanhnd <ducthanhnguyen95@gmail.com>
2025-03-17 23:13:33 +0700

    Date

---
+++ objects/date.js
@@ -0,0 +1,9 @@
+const now = new Date();
+const date1 = new Date('May 11 2025 09:00');
+const date2 = new Date(2025, 4, 11, 9);
+
+console.log(now.toDateString());
+console.log(now.toTimeString())
+console.log(now.toISOString())
+console.log(date1);
+console.log(date2);
