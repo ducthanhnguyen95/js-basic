14526ed3a9cf132773392b50370549a9970fabb9
thanhnd <ducthanhnguyen95@gmail.com>
2025-03-22 23:29:11 +0700

    Blog Post Object

--- objects/exercise-3-object-equality.js
+++ objects/exercise-3-object-equality.js
@@ -4,9 +4,11 @@ function Address(street, city, zipCode) {
   this.zipCode = zipCode;
 }
 
+
 let address1 = new Address('10 Downing', 'London', 'SW1A 2AA');
 let address2 = new Address('10 Downing', 'London', 'SW1A 2AA');
 let address3 = address1;
+
 
 function areEqual(address1, address2) {
   return (
@@ -16,9 +18,11 @@ function areEqual(address1, address2) {
   );
 }
 
+
 function areSame(address1, address2) {
   return address1 === address2;
 }
+
 
 console.log(areEqual(address1, address2));
 console.log(areSame(address1, address2));
