e692bb35ed03bedfb525d0bd629c11cafa699cd3
thanhnd <ducthanhnguyen95@gmail.com>
2025-04-05 22:29:20 +0700

    Changing this

---
+++ functions/changing-this.js
@@ -0,0 +1,47 @@
+// const video = {
+//   title: 'a',
+//   tags: ['a', 'b', 'c'],
+//   showTags() {
+//     const self = this;
+//     this.tags.forEach(function (tag) {
+//       console.log(self.title, tag);
+//     });
+//   },
+// };
+// video.showTags();
+// ––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+// function playVideo(a, b) {
+//   console.log(this);
+// }
+
+// playVideo.call({ name: 'Thanh' }, 1, 2);
+// playVideo.apply({ name: 'Thanh' }, [1, 2]);
+// playVideo.bind({ name: 'Thanh' })();
+
+// playVideo();
+// ––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+// const video = {
+//   title: 'a',
+//   tags: ['a', 'b', 'c'],
+//   showTags() {
+//     this.tags.forEach(
+//       function (tag) {
+//         console.log(this.title, tag);
+//       }.bind(this)
+//     );
+//   },
+// };
+// video.showTags();
+// ––––––––––––––––––––––––––––––––––––––––––––––––––––
+const video = {
+  title: 'a',
+  tags: ['a', 'b', 'c'],
+  showTags() {
+    this.tags.forEach((tag) => {
+      console.log(this.title, tag);
+    });
+  },
+};
+video.showTags();
